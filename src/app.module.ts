84ce6671bdbfc9d6688f17793b32d7f19c1fe7be
Rafael LS <rafa.rls.rls@gmail.com>
2023-02-11 22:14:58 -0600

    dev-auth ADD module to authentication created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { UsersModule } from './users/users.module';
     }),
     MikroOrmModule.forRoot({
     }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
