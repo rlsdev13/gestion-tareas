4de998d88ce325c17e33f371e9415411880c583e
Rafael LS <rafa.rls.rls@gmail.com>
2023-02-11 20:34:01 -0600

    dev-users ADD logic to list all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor( private readonly usersService : UsersService){}
+
+    @Get()
+    async getAllUsers(@Query('limit') limit : string, @Query('offset') offset : string){
+        return await this.usersService.getAllUsers(Number(limit),Number(offset));
+    }
+
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,17 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { Users } from './users.entity';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MikroOrmModule.forFeature([
-      
+      Users
     ])
   ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports:[UsersService]
 })
 export class UsersModule {}
